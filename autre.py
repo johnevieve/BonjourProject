675377adcb5a0f8ea763967b3b12c5388db6232d
genev <genevievecote10@gmail.com>
2021-11-10 20:58:14 -0500

    Réusinages

--- autre.py
+++ autre.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+"""
+Autre script pour appeler les fonctions
+du script principal.
 
+Par: Genevieve Trudel
+"""
 # Importer le module main (fichiewr main.py).
 # On le renomme bonjour pour éviter le conflit
 # de nom avec la fonction main plus bas.
@@ -7,6 +12,9 @@ import main as bonjour
 
 
 def main():
+    """
+    Fonction principale du script
+    """
     bonjour.bonjour('Genevieve T. alias GT')
     bonjour.compter(8, 0.5)
 
--- autre2.py
+++ autre2.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
+"""
+Autre script pour appeler les fonctions
+du script principal 2.
 
+Par: Genevieve Trudel
+"""
 # Importer directement les fonctions.
 from main import bonjour, compter
 
 
 def main():
+    """
+    Fonction principale du script
+    """
     bonjour('Genevieve T.')
     compter(3)
 
